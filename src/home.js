f4d78b8a435ce1e38b0694a2882f0c485d0f84f4
Natalia_Filtanovych <natashe4ka21092016@gmail.com>
2026-03-21 19:46:32 +0200

    update modal

--- src/home.js
+++ src/home.js
@@ -1,7 +1,13 @@
 //Логіка сторінки Home
-import { handleCategory, handlerLoadMore, initHomePage } from './js/handlers';
+import {
+  handleCategory,
+  handlerLoadMore,
+  handlerModal,
+  initHomePage,
+} from './js/handlers';
 import { refs } from './js/refs';
 
 document.addEventListener('DOMContentLoaded', initHomePage);
 refs.loadMoreBtn.addEventListener('click', handlerLoadMore);
 refs.categories.addEventListener(`click`, handleCategory);
+refs.products.addEventListener('click', handlerModal);
--- src/js/render-function.js
+++ src/js/render-function.js
@@ -57,5 +57,35 @@ export function hideNotFound() {
   refs.notFound.classList.remove('not-found--visible');
 }
 export function clearProductList() {
-  refs.products.innerHTML = "";
+  refs.products.innerHTML = '';
+}
+
+export function renderModal({
+  thumbnail,
+  title,
+  tags,
+  description,
+  shippingInformation,
+  returnPolicy,
+  price,
+}) {
+  const markup = `<img class="modal-product__img" src="${thumbnail}" alt="${title}" />
+      <div class="modal-product__content">
+        <p class="modal-product__title">${title}</p>
+        <ul class="modal-product__tags">${tags}</ul>
+        <p class="modal-product__description">${description}</p>
+        <p class="modal-product__shipping-information">Shipping:${shippingInformation}</p>
+        <p class="modal-product__return-policy">Return Policy:${returnPolicy}</p>
+        <p class="modal-product__price">Price:${price} $</p>
+        <button class="modal-product__buy-btn" type="button">Buy</button>
+      </div>`;
+  refs.modalProduct.insertAdjacentHTML('beforeend', markup);
+}
+
+export function showModal() {
+  refs.modal.classList.add('modal--is-open');
+}
+
+export function hideModal() {
+  refs.modal.classList.remove('modal--is-open');
 }
